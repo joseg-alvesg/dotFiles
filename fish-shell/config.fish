da8e9ad09e61e55cab2180ce6c72a139aaafd8d6
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-23 17:22:14 -0300

    status

---
+++ fish-shell/config.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+	source $fish_function_path/git_alias.fish
+	source $fish_function_path/git_func.fish
+end
--- fish-shell/git_alias.fish
+++ fish-shell/git_alias.fish
@@ -1,7 +1,7 @@
-
 alias ga="git add"
 alias gaa="git add --all"
 alias gacaula="git add --all && git commit -m 'aula'"
 alias gcmsg="git commit -m"
+alias gst="git status"
 alias gp="git push"
 alias gl="git pull"
