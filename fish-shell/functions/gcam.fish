c269dfb2ce83c709c1b2be8d92079c895de91002
joseg-alvesg <cinhoalves85@gmail.com>
2023-07-02 19:29:26 -0300

    update: fish functions

---
+++ fish-shell/functions/gcam.fish
@@ -0,0 +1,3 @@
+function gcam --description 'git commit -am'
+git commit -am $argv
+end
--- fish-shell/functions/utility_func.fish
+++
@@ -1,9 +0,0 @@
-alias rain="curl http://wttr.in/\?lang=pt-br"
-
-function rainf
-	curl http://wttr.in/$argv\?lang=pt-br
-end
-
-function raini
-	curl http://wttr.in/previsao.png\?lang=pt-br --output previsao.png
-end
