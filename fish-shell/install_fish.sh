e1a5fdbeed2ae6a42a648f169e7dd11be03cf8fb
joseg-alvesg <cinhoalves85@gmail.com>
2023-07-04 10:42:59 -0300

    update

--- fish-shell/install_fish.sh
+++ fish-shell/install_fish.sh
@@ -1,21 +1,20 @@
 # check if i in arch or deb
 if [ -f /etc/debian_version ]; then
-    apt-add-repository ppa:fish-shell/release-3
-    apt-get update
-    apt-get install fish
+    sudo apt-add-repository ppa:fish-shell/release-3
+    sudo apt-get update
+    sudo apt-get install fish
 elif [ -f /etc/arch-release ]; then
-    pacman -Syy --needed --noconfirm git neovim which fish 
+    sudo pacman -Syy --needed --noconfirm git neovim which fish 
     curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install
     chmod +x ./install
     fish install --noninteractive --path=~/.local/share/omf --config=~/.config/omf
-    chsh -s $(which fish) 
+    sudo chsh -s $(which fish) 
     rm ./install
 fi
 mkdir -p ~/.config/fish/config.d/
 mkdir -p ~/.config/fish/functions/
 
-fish -c "omf install agnoster"
-cp -r ~/dotFiles/fish-shell/themes/agnoster/ ~/.local/share/omf/themes/
+fish -c "git clone https://github.com/joseg-alvesg/theme-agnoster-fork  "
 
 fish -c "omf install fish-spec bak foreign-env config nvm ssh-config.d"
 
