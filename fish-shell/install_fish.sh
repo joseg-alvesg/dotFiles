67a2b8212c25e1a7039fe16adcdb07e8dbd11316
joseg-alvesg <cinhoalves85@gmail.com>
2023-07-05 22:17:16 -0300

    update

--- fish-shell/install_fish.sh
+++ fish-shell/install_fish.sh
@@ -24,19 +24,19 @@ curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install 
 chmod +x ./install
 fish install --noninteractive --path=~/.local/share/omf --config=~/.config/omf
 sudo chsh -s $(which fish) 
-rm ./install
+rm -v ./install
 
 
-mkdir -p $FISH_CONFIG/config.d/
-mkdir -p $FISH_CONFIG/functions/
+mkdir -pv $FISH_CONFIG/config.d/
+mkdir -pv $FISH_CONFIG/functions/
 
 fish -c "omf install https://github.com/joseg-alvesg/theme-agnoster-fork"
 mv -v $OMF_PATH/pkg/agnoster-fork $OMF_PATH/themes/
 
 fish -c "omf install fish-spec bak foreign-env config nvm ssh-config.d"
 
-cp ~/dotFiles/fish-shell/config.fish $FISH_CONFIG/config.fish
-cp -r ~/dotFiles/fish-shell/config.d/* $FISH_CONFIG/config.d/
-cp -r ~/dotFiles/fish-shell/functions/* $FISH_CONFIG/functions/
+cp -v ~/dotFiles/fish-shell/config.fish $FISH_CONFIG/config.fish
+cp -rv ~/dotFiles/fish-shell/config.d/* $FISH_CONFIG/config.d/
+cp -rv ~/dotFiles/fish-shell/functions/* $FISH_CONFIG/functions/
 
 fish
