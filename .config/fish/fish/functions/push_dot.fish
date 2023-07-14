092f0f7ddd4c26ca1557936de2f99f69b4e691b5
joseg-alvesg <cinhoalves85@gmail.com>
2023-07-13 22:35:59 -0300

    update

--- .config/fish/fish/functions/push_dot.fish
+++ .config/fish/fish/functions/push_dot.fish
@@ -1,15 +1,4 @@
 function push_dot
-  cd $HOME/.local/share/omf/themes/agnoster-fork/
-  if test -z $argv
-    git add .
-    git commit -m "update"
-    git push
-  else
-    git add --all
-    git commit -m $argv
-    git push
-  end
-
   cd $HOME/dotFiles/
   if test -z $argv
     git add .
