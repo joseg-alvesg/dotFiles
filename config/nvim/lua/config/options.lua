17ba704ad782ef3b5b0cf76e38033f8bbb2c228c
joseg-alvesg <cinhoalves85@gmail.com>
2024-03-06 23:31:12 -0300

    update

---
+++ config/nvim/lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+--
+-- Add any additional options here
+vim.g.mkdp_browser = "electron"
+-- vim.g.mkdp_port = "8959"
+vim.g.mkdp_theme = "dark"
